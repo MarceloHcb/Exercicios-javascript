fc1ea58a6cf8c52282ad6723b8d15790535852b6
MarceloHcb <marcelohcb@msn.com>
2022-09-21 15:57:56 -0300

    envia tipos primitivos Dinâmica e Operações Aritméticas

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ let birthYear = 1981
 // birthYear = 2030
 console.log("Meu nome é " + myName+ ", minha cidade natal é "+ birthCity+ ", eu nasci em " + birthYear )
 
+let movie = "Avengers"  //string literal
+let score = 10.89  // number literal
+let isValid = true  // boolean
+let name; // undefined
+let color = null //redefinição
 
+
+// +, - , * , /, **
+
+let salary = 3500
+// console.log(salary + salary)
+// console.log(salary - salary)
+// console.log(salary * salary)
+// console.log(salary / salary)
+// console.log(3**3)
+
+salary ++
+console.log(salary)
