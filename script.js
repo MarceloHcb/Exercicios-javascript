98d76f3ba33d78d55d1384b0732b76b5b7489ce4
MarceloHcb <marcelohcb@msn.com>
2022-09-21 15:50:05 -0300

    atualiza variaveis

---
+++ script.js
@@ -0,0 +1,9 @@
+// VARIÁVEIS
+const myName = "Marcelo"
+const birthCity = "Rio de Janeiro"
+let birthYear = 1981
+// birthCity = "sp"
+// birthYear = 2030
+console.log("Meu nome é " + myName+ ", minha cidade natal é "+ birthCity+ ", eu nasci em " + birthYear )
+
+
