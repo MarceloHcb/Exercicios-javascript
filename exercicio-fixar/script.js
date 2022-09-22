1df4e20f2310c71b93ee4451dae33499867a01ff
MarceloHcb <marcelohcb@msn.com>
2022-09-22 11:25:44 -0300

    atualizando exercicios

--- exercicio-fixar/script.js
+++ exercicio-fixar/script.js
@@ -4,3 +4,74 @@ const height = 8
 const area = base * height
 console.log(area)
 
+// IF ELSE
+
+const notaPessoaCandidata = 40
+
+if(notaPessoaCandidata > 80){
+    console.log("parabéns você foi aprovada")
+}
+else if (notaPessoaCandidata < 80 && notaPessoaCandidata >= 60){
+    console.log("Você está na nossa lista de espera")
+}
+else{
+    console.log("Você foi reprovado")
+}
+// OPERADORES LÓGICOS
+
+const currentHour = 13;
+let message;
+
+if(currentHour >= 22 ){
+    message = "Não deveríamos comer nada, é hora de dormir";
+}
+else if (currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar :D"
+}
+else if (currentHour >= 14 && currentHour <18){
+    message = "Vamos fazer um bolo pro café da tarde"
+}
+else if (currentHour>11 && currentHour<14){
+    message = "Hora do almoço!!!"
+}
+else if (currentHour > 4 && currentHour < 11){
+    message = "Hmmm, cheiro de café recém-passado"
+}
+
+console.log(message)
+
+
+
+console.log(!(2 + 2) === 4);
+console.log(!null);
+console.log(!undefined);
+
+// SWITCH / CASE
+
+mes = 'setembro';
+let estacaoDoAno = '?';
+
+switch (mes) {
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+        estacaoDoAno = 'Verão';
+        break;
+    case 'abril':
+    case 'maio':
+    case 'junho':
+        estacaoDoAno = 'Outono';
+        break;
+    case 'julho':
+    case 'agosto':
+    case 'setembro':
+        estacaoDoAno = 'Inverno';
+        break;
+    case 'outubro':
+    case 'novembro':
+    case 'dezembro':
+        estacaoDoAno = 'Primavera';
+    default:
+        console.log("insira um mês do ano")
+}
+console.log(estacaoDoAno)
