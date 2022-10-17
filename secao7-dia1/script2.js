5aa3a13282eab14fca39bb43d751213f1296a2f4
MarceloHcb <marcelohcb@msn.com>
2022-10-17 20:44:57 -0300

    atualiza exercicios 7.1

--- secao7-dia1/script2.js
+++ secao7-dia1/script2.js
@@ -16,9 +16,17 @@ console.log(fatorial(4));
 function aconteceu(){
     let longestWord ='Antônio foi ao banheiro e não sabemos o que aconteceu'
 const nova = longestWord.split(' ')
-console.log(nova);
-nova.forEach(n =>{
-    console.log(n.length);
-})
+
+let maxLength = ""
+for (let index = 0; index < nova.length; index++) {
+    
+    nova.forEach(n =>{
+        if (n.length > nova[index].length){
+            maxLength = n
+        }
+    })
+    }
+    console.log(maxLength);
 }
+
 aconteceu()
