d4bb0482256be3d09c6970d2069f9aa930dc80ab
MarceloHcb <marcelohcb@msn.com>
2022-10-24 13:21:12 -0300

    atualizando exercicios js

---
+++ exercicio-fixar/hofs/script.js
@@ -0,0 +1,15 @@
+function acordando() {
+    return 'Acordando'
+}
+function boraTomarCafe() {
+    return 'Bora tomar café!!';
+}
+function partiuDormir() {
+    return 'Partiu dormir!!';
+}
+
+const doingThings = (param) => {
+    
+    console.log(param);
+} 
+doingThings([acordando(), boraTomarCafe(), partiuDormir()])
