a2553ddc5c8d0464aeb920717893474b5a4af35d
MarceloHcb <marcelohcb@msn.com>
2022-10-26 11:51:55 -0300

    atualizando exercicios

--- mentoria/25-10-Hofs/1-countCharacters.js
+++ mentoria/25-10-Hofs/1-countCharacters.js
@@ -1,9 +1,9 @@
-
+const data = require('./database')
 
 //1 - Crie uma função que mostre a quantidade de personagens
 
-const countCharacters = () => {
-  
+const countCharacters = () => { 
+  return data.characters.length
 }
 
 console.log(countCharacters());
--- mentoria/25-10-Hofs/4-isAndroide-some-find.js
+++ mentoria/25-10-Hofs/4-isAndroide-some-find.js
@@ -1,9 +1,13 @@
-
+const data = require("./database")
 
 //6 - Imprimir se existe algum personagem de uma determinada espécie. True ou False. Caso o retorno seja True, traga os nomes de todos os personagens desta espécie.
 
-const isAndroide = () => {
-  
+const isAndroide = (specie) => {
+    let names = []
+    if(data.species.some((specieSome) => specieSome.name === specie)){
+        data.species.find((el)=> names.push(el) )
+    }
+    return names
 }
 
 console.log(isAndroide("Saiyans"));
