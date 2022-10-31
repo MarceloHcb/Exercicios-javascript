63a6b3cc221a39b2323f19a98608758fff814e6a
MarceloHcb <marcelohcb@msn.com>
2022-10-30 23:30:21 -0300

    atualiza exercicios js secao8.4 e 8.5

--- secao8-dia4/reuce-filter.js
+++ secao8-dia4/reuce-filter.js
@@ -1,11 +1,9 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
-
 const pares =numbers.filter((number) => number % 2 === 0)
 const resultado = pares.reduce((acc,curr) => acc + curr,0)
 console.log(pares);
 console.log(resultado);
-
 
 
 const getEven = (number) => number % 2 === 0;
@@ -16,16 +14,12 @@ const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que
 console.log(sumNumbers(numbers)); // 152
 
 
-
 // SOLUCAO USANDO O REDUCE 
-
-
 
 const sumPairr = (accumulator, number) => (
   (number % 2 === 0) ? accumulator + number : accumulator
 );
 
 const sumNumberss = (array) => array.reduce(sumPairr, 0);
-
 console.log(sumNumberss(numbers)); // 152
 
